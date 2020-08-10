f2b996aca307b7062eaacf9f78b591ebc14129a3
TonySeo <shtelnd1@naver.com>
2020-08-10 21:01:32 +0900

    Initial commit

---
+++ app/src/main/java/com/tonyseo/ex44fragment2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.tonyseo.ex44fragment2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click(View view) {
+
+    }
+}
